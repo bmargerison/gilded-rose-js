47e16eaccb261b4eda523f077ab3b19807617dba
bmargerison <bmargerison@outlook.com>
2022-01-06 10:51:11 +0000

    Test passing - quality decreases by 1

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -1,9 +1,10 @@
 const {Shop, Item} = require("../src/gilded_rose");
 
 describe("Gilded Rose", function() {
-  it("should foo", function() {
-    const gildedRose = new Shop([new Item("foo", 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("fixme");
+  it("increases quality by 1", function() {
+    const shop = new Shop([new Item("item", 20, 1)]);
+    const items = shop.updateQuality();
+    expect(items[0].quality).toBe(0);
   });
 });
+
