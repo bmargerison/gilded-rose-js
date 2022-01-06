cd6cf76967984b8493f653a8603ba82b983a3f0a
bmargerison <bmargerison@outlook.com>
2022-01-06 15:27:09 +0000

    Extract remaining Backstage Passes logic

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -21,6 +21,9 @@ class Shop {
 
       if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
         this.items[i].sellIn = this.items[i].sellIn - 1;
+        if (this.items[i].sellIn < 0) {
+          this.items[i].quality = this.items[i].quality - this.items[i].quality;
+        };
         if (this.items[i].quality < 50) {
           this.items[i].quality = this.items[i].quality + 1;
             if (this.items[i].sellIn < 11) {
@@ -34,6 +37,9 @@ class Shop {
               }
             }
         }
+        if (this.items[i].sellIn < 0) {
+          this.items[i].quality = this.items[i].quality - this.items[i].quality;
+        };
       }
 
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
@@ -48,7 +54,7 @@ class Shop {
         }
       }
 
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros' && this.items[i].name != 'Aged Brie') {
+      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros' && this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
 
@@ -60,8 +66,6 @@ class Shop {
                 this.items[i].quality = this.items[i].quality - 1;
               }
             }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
           }
         }
       }
